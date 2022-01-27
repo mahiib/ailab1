dfb363a98d289e41328ec4aec62a3c8e46b8dde2
Mahesh B <61156481+mahiib@users.noreply.github.com>
2022-01-27 22:33:25 +0530

    Update tictactoe2player.py

--- tictactoe2player.py
+++ tictactoe2player.py
@@ -1,105 +1,77 @@
-theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
-            '4': ' ' , '5': ' ' , '6': ' ' ,
-            '1': ' ' , '2': ' ' , '3': ' ' }
+#TicTacToe
+board = {
+    '1': ' ', '2': ' ', '3': ' ', '4': ' ', '5': ' ', '6': ' ', '7': ' ', '8': ' ', '9': ' '
+}
 
-board_keys = []
 
-for key in theBoard:
-    board_keys.append(key)
-
-''' We will have to print the updated board after every move in the game and 
-    thus we will make a function in which we'll define the printBoard function
-    so that we can easily print the board everytime by calling this function. '''
-
-def printBoard(board):
-    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+def printBoard():
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
     print('-+-+-')
     print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('-+-+-')
-    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
 
-# Now we'll write the main function which has all the gameplay functionality.
 def game():
-
+    print('Game starts now')
     turn = 'X'
     count = 0
-
-
     for i in range(100):
-        printBoard(theBoard)
-        print("It's your turn," + turn + ".Move to which place?")
-
-        move = input()
-              
-
-        if theBoard[move] == ' ':
-            theBoard[move] = turn
+        print('')
+        print("It is {}'s turn".format(turn))
+        printBoard()
+        move = input('Enter the position:')
+            
+        if board[move] == ' ':
+            board[move] = turn
             count += 1
-        else:
-            print("That place is already filled.\nMove to which place?")
+            
+        elif board[move] != ' ':
+            print("Aldready filled")
             continue
-
-        # Now we will check if player X or O has won,for every move after 5 moves. 
+            
         if count >= 5:
-            if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")                
+            if ( board['1'] == board['2'] == board['3'] != ' '):
+                print('{} won the match'.format(turn))
                 break
-            elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ': # across the middle
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
+            if ( board['4'] == board['5'] == board['6'] != ' '):
+                print('{} won the match'.format(turn))
                 break
-            elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ': # across the bottom
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
+            if ( board['7'] == board['8'] == board['9'] != ' '):
+                print('{} won the match'.format(turn))
                 break
-            elif theBoard['1'] == theBoard['4'] == theBoard['7'] != ' ': # down the left side
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
+            if ( board['1'] == board['4'] == board['7'] != ' '):
+                print('{} won the match'.format(turn))
                 break
-            elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ': # down the middle
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
+            if ( board['3'] == board['6'] == board['9'] != ' '):
+                print('{} won the match'.format(turn))
                 break
-            elif theBoard['3'] == theBoard['6'] == theBoard['9'] != ' ': # down the right side
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break 
-            elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ': # diagonal
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
+            if ( board['2'] == board['5'] == board['8'] != ' '):
+                print('{} won the match'.format(turn))
                 break
-            elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ': # diagonal
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break 
-
-        # If neither X nor O wins and the board is full, we'll declare the result as 'tie'.
+            if ( board['1'] == board['5'] == board['9'] != ' '):
+                print('{} won the match'.format(turn))
+                break
+            if ( board['3'] == board['5'] == board['7'] != ' '):
+                print('{} won the match'.format(turn))
+                break
+        
         if count == 9:
-            print("\nGame Over.\n")                
-            print("It's a Tie!!")
-
-        # Now we have to change the player after every move.
-        if turn =='X':
+            print("Game tie")
+            break
+            
+        if turn == 'X':
             turn = 'O'
         else:
-            turn = 'X'        
-    
-    # Now we will ask if player wants to restart the game or not.
-    restart = input("Do want to play Again?(y/n)")
-    if restart == "y" or restart == "Y":  
-        for key in board_keys:
-            theBoard[key] = " "
-
+            turn = 'X'
+    print('Do you want to restart the game?[Y/N]')
+    choice = input()
+    if choice == 'y' or choice == 'Y':
+        for key in board:
+            board[key] = ' '
+        print('New Board')
+        printBoard()
         game()
+            
 
-if __name__ == "__main__":
-    game()
+game()
+printBoard()
